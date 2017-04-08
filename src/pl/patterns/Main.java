67fe32d0942695e686ebff6145d380b886e737f1
Bartłomiej Polaszewski <bartlomiej.polaszewski@gmail.com>
2017-04-08 12:18:19 +0200

    initial commit

---
+++ src/pl/patterns/Main.java
@@ -0,0 +1,9 @@
+package pl.patterns;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ src/pl/strategy/Test.java
@@ -0,0 +1,9 @@
+package pl.strategy;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
