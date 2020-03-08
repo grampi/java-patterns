0cb1fa8bb9dd785a9ac2e9e22682ba8fa8659fc2
Bartłomiej Polaszewski <bartlomiej.polaszewski@gmail.com>
2020-03-08 18:08:16 +0100

    Flyweight pattern

---
+++ src/pl/structural/flyweight/Forest.java
@@ -0,0 +1,25 @@
+package pl.structural.flyweight;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+
+public class Forest extends JFrame {
+
+    private final List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        trees.forEach((tree) -> {
+            tree.draw(graphics);
+        });
+    }
+}
